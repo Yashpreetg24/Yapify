0f1b416c40082a1a698222548999d26186f0de09
Yashpreet <yashpreetg2024@gmail.com>
2026-04-13 00:54:38 +0530

    few changes

--- src/components/LeftSideBar/LeftSideBar.jsx
+++ src/components/LeftSideBar/LeftSideBar.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './LeftSideBar.css'
+import assets from '../../assets/assets'
 
 const LeftSideBar = () => {
   return (
-    <div>
-      
+    <div className='ls'>
+      <div className="ls-top">
+        <div className="ls-nav">
+            <img src={assets.logo} className='logo' alt=''></img>
+            <div className="menu">
+                <img src={assets.menu_icon} alt=''></img>
+            </div>
+        </div>
+        <div className="ls-search">
+            <img src={assets.search_icon} alt=''></img>
+            <input type='text' placeholder='Search here..'></input>
+        </div>
+      </div>
     </div>
   )
 }
