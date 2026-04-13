f3dfa7a95e22eb9499aa264ca0e94dc3035a8f19
Yashpreet <yashpreetg2024@gmail.com>
2026-04-13 11:45:06 +0530

    top and bottom part of the chatbox

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './ChatBox.css'
+import assets from '../../assets/assets'
 
 const ChatBox = () => {
   return (
-    <div>
-      
+    <div className='chat-box'>
+      <div className="chat-user">
+        <img src={assets.profile_img} alt= ''></img>
+        <p>Richard Sanford <img className='dot' src={assets.green_dot} alt=''></img></p>
+        <img src={assets.help_icon} className='help' alt=''></img>
+      </div>
+      <div className="chat-input">
+        <input type='text' placeholder='Send a message'></input>
+        <input type='file' id='image' accept='image/png, image/jpeg' hidden></input>
+        <label htmlFor='image'>
+            <img src={assets.gallery_icon} alt=''></img>
+        </label>
+        <img src={assets.send_button} alt=''></img>
+      </div>
     </div>
   )
 }
